9485890f301775c1099dafeeae6f8e6ab5d1a3a8
Kjubyy <imkjuby2410@gmail.com>
2023-03-05 01:08:52 +0100

    initial commit

---
+++ download_youtube_video.py
@@ -0,0 +1,13 @@
+import youtube_dl
+
+link = input("Bitte geben den Youtube Link ein: ")
+
+url = link
+
+ydl_opts = {
+    'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+    'outtmpl': '%(title)s.%(ext)s'
+}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
